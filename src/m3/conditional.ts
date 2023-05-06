ea7539ecddd8c011d6a4d92269dda3e934f323c6
mr7aali <mr7aali@gmail.com>
2023-05-06 15:14:38 +0600

    a

--- src/m3/conditional.ts
+++ src/m3/conditional.ts
@@ -2,3 +2,33 @@
 
 
 type a1 = string;
+type a3 = undefined;
+type a4 = number;
+type a2 = a1 extends string ? string : null;
+
+
+//nested conditonal type 
+type d = a1 extends null
+    ? null
+    : a3 extends number
+    ? number
+    : a4 extends null
+    ? null
+    : never;
+
+
+
+type Sheikh = {
+    wife1: string;
+    wife2: string;
+}
+
+type CheckProperty<t, k> = k extends keyof Sheikh ? true : false;
+
+type Ckeck = CheckProperty<Sheikh, "wife1">;
+
+type Bandopi = "X" | "Y" | "Z";
+
+type RemoveBandopi<T,K>= T extends K ? never : T;
+
+type CurrentBandopi = RemoveBandopi<Bandopi,"Z">;
